9ab40b7d1d1eda9caa55930bef973f36f70f4525
William Montesedoca <wvmon360@gmail.com>
2017-07-03 19:24:35 -0600

    Modified table

--- ContosoBook/ContosoBook/Book.sql
+++ ContosoBook/ContosoBook/Book.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [dbo].[Book]
 (
 	[Id] INT IDENTITY (1, 1) NOT NULL, 
-    [Title] NCHAR(50) NULL, 
-    [AuthorName] NCHAR(50) NULL, 
-    [Year] INT NULL,
+	[Title] NCHAR(50) NULL, 
+	[AuthorName] NCHAR(50) NULL, 
+	[Year] INT NULL,
+	[Pages] INT NULL,
 	PRIMARY KEY CLUSTERED ([Id] ASC)
 )
